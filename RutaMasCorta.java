35bf19427462eab5fc3a49bdc24483787a3b6a71
Leonardo Mejía <leomejia646@gmail.com>
2024-05-05 22:33:50 -0600

    Cambios

--- RutaMasCorta.java
+++ RutaMasCorta.java
@@ -1,7 +1,7 @@
 public class RutaMasCorta implements RutaStrategy {
     public String calcularRuta(String puntoA, String puntoB) {
         System.out.println("Calculando distancia...");
-        System.out.println("Verificando ");
+        System.out.println("Verificando desvíos");
         System.out.println("Confirmando metros recorridos");
 
         return "Ruta más Corta entre " + puntoA + " y " + puntoB + "es utilizando la calzada con automovil";
